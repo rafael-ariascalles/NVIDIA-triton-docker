2a3870f120214bd2415b4ab93e49ed01836baed1
rafael-ariascalles <rafael-ariascalles@github.com>
2022-10-13 21:25:15 -0600

    <doc> implementation of docker and docker compose

---
+++ main.py
@@ -0,0 +1,17 @@
+from starlette.responses import StreamingResponse
+from fastapi import FastAPI, File, UploadFile
+import requests
+import os
+#We generate a new FastAPI app in the Prod environment
+#https://fastapi.tiangolo.com/
+
+app = FastAPI("Triton Health Check")
+
+@app.get("/")
+#Call your get function for a health Check
+async def healthcheck():
+    #to receive both (face-bokeh and face-emotion)
+    service_boke = requests.get(os.getenv('SERVICE_BOKE'))
+    service_emotion = requests.get(os.getenv('SERVICE_EMOTION'))
+
+    return "OK"
