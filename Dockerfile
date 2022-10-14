2a0f35e9dbe8969a614902fdb6e790ef07fdd8df
EC2 Default User <ec2-user@ip-172-31-152-65.ec2.internal>
2022-10-14 06:04:45 +0000

    <code> Implementation of thhe entrypoint of the app

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.8
 COPY requirements.txt  .
 RUN  pip3 install -r requirements.txt
+ENV SERVICE_BOKE "service:80"
+ENV SERVICE_EMOTION "service:80"
 COPY . .
 CMD ["uvicorn", "main:app","--host", "0.0.0.0"]
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 #We generate a new FastAPI app in the Prod environment
 #https://fastapi.tiangolo.com/
 
-app = FastAPI("Triton Health Check")
+app = FastAPI(title="Triton Health Check")
 
 @app.get("/")
 #Call your get function for a health Check
@@ -13,5 +13,5 @@ async def healthcheck():
     #to receive both (face-bokeh and face-emotion)
     service_boke = requests.get(os.getenv('SERVICE_BOKE'))
     service_emotion = requests.get(os.getenv('SERVICE_EMOTION'))
-
-    return "OK"
+    
+    return {"boke":service_boke.json(),"emotion":service_emotion.json()}
